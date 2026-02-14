df3a6146ff04d8877dd5bef93933ea35c2efb589
huanzihong <1165565243@qq.com>
2026-02-14 13:45:49 +0800

    feat(tag):新增gameplaytag

---
+++ Source/HZDemo/GAS/HZGameplayTags.cpp
@@ -0,0 +1,9 @@
+﻿#include "HZGameplayTags.h"
+
+namespace FGGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+		SmartObject_Health, "SmartObject.Health", "治疗");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+		SmartObject_Ammo, "SmartObject.Ammo", "子弹");
+}
---
+++ Source/HZDemo/GAS/HZGameplayTags.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include "NativeGameplayTags.h"
+namespace HZGameplayTags
+{
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(SmartObject_Health);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(SmartObject_Ammo);
+}
